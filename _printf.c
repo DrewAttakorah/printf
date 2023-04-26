7f0467ced229b4f62a25402cc7eda327e80f310a
DrewAttakorah <andyaw306@gmail.com>
2023-04-25 18:45:01 -0700

    Printf commit

--- _printf.c
+++ _printf.c
@@ -10,48 +10,49 @@
 * specifiers contained into format
 * Return: length of the formatted output string
 */
+
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    va_list args;
-    va_start(args, format);
-
-    for (const char *ptr = format; *ptr != '\0'; ptr++)
-    {
-        if (*ptr == '%')
+	int count = 0;
+	va_list args;
+	va_start(args,format);
+	
+	for (const char *ptr = format; *ptr != '\0'; ptr++)
 	{
-            ptr++;
-            switch (*ptr)
-	    {
-                case 'c': {
-                    char c = (char) va_arg(args, int);
-                    putchar(c);
-                    count++;
-                    break;
-                }
-                case 's': {
-                    const char *str = va_arg(args, const char*);
-                    for (; *str != '\0'; str++) 
-		    {
-                        putchar(*str);
-                        count++;
-                    }
-                    break;
-                }
-                case '%': {
-                    putchar('%');
-                    count++;
-                    break;
-                }
-                default:
-                    break;
-            }
-        } else {
-            putchar(*ptr);
-            count++;
-        }
-    }
-
-    va_end(args);
-    return count;
+		if (*ptr == '%')
+		{
+			ptr++;
+			switch (*ptr){
+				case 'c': 
+					{
+						char c = (char) va_arg(args, int);
+						putchar(c);
+						count++;
+						break;
+					}
+				case 's': {
+						  const char *str = va_arg(args, const char*);
+						  for (; *str != '\0'; str++)
+						  {
+							  putchar(*str);
+							  count++;
+						  }
+						  break;
+					  }
+				case '%': {
+						  putchar('%');
+						  count++;
+						  break;
+					  }
+				default:
+					  break;
+			}
+		}
+		else {
+			putchar(*ptr);
+			count++;
+		}
+	}
+	va_end(args);
+	return count;
 }
