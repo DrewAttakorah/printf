dc7449346fcaa74db39126613111f48e51e5456d
nhii07 <stannan3107@gmail.com>
2023-04-29 15:41:44 -0700

    Update _print.c

---
+++ _print.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * get_print - will select the appropriate printing function
+ * depending on the conversion specifier passed to _printf
+ * @s: character that holds the conversion specifier
+ * Description: the function will loop through the structs array
+ * funct_arr[] to find a match between the specifier passed to _printf
+ * and the first element of the struct, and then the approriate
+ * printing function
+ * Return: a pointer to the matching printing function
+ */
+
+int (*get_print(char z))(va_list, flags_t *)
+{
+	/** struct array that maps specifiers to function */
+	phandler funct_arr[] = {
+		{'i', print_int},
+		{'s', print_string},
+		{'c', print_char},
+		{'d', print_int},
+		{'u', print_unsigned},
+		{'x', print_hex},
+		{'X', print_hex_big},
+		{'b', print_binary},
+		{'o', print_octal},
+		{'R', print_rot13},
+		{'r', print_rev},
+		{'S', print_bigS},
+		{'p', print_address},
+		{'%', print_percent}
+		};
+
+	int flags = 14;
+
+	register int i;
+
+	/** Loop through the array to find a match and return appro funct. */
+	int i = 0;
+	while (i <flags)
+       	{
+		if (funct_arr[i].c == z) 
+		{
+			return funct_arr[i].f;
+		}
+	i++;
+	}
+	return NULL;
+}
