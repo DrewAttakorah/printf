633c6ac429f7fd02055a585c5d24fa26d24f6f6d
DrewAttakorah <andyaw306@gmail.com>
2023-05-07 12:27:03 +0000

    update
    update

--- get_print.c
+++ get_print.c
@@ -38,8 +38,8 @@ int (*get_print(char s))(va_list, flags_t *)
 	/** While loop through the array to find a match and return appro funct. */
 	i = 0;
     while (i < flags) {
-        if (func_arr[i].c == s) {
-            return (func_arr[i].f);
+        if (funct_arr[i].c == s) {
+            return (funct_arr[i].f);
         }
         i++;
     }
